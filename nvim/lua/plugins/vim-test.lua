eddcff2a21e005be25c538c87926c51861fa3374
Michael Joseph Kramer <michael@sage.house>
2022-04-10 09:23:40 -0400

    Update vim-test configuration
    Updates the vim-test configuration:
    
    - Sets the test#strategy to the default
    - Updates the mapping to TestSuite
    - Adds a TestVisit mapping

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -2,14 +2,7 @@
 -- ALIASES
 --------------------------------------------------------------------------------
 
-local g = vim.g
 local map = vim.api.nvim_set_keymap
-
---------------------------------------------------------------------------------
--- CONFIGURATION
---------------------------------------------------------------------------------
-
-g["test#strategy"] = "neovim" -- default strategy
 
 --------------------------------------------------------------------------------
 -- MAPPINGS
@@ -17,6 +10,7 @@ g["test#strategy"] = "neovim" -- default strategy
 
 map('n', 't<C-n>', ':noautocmd wa<cr> :TestNearest<CR>', { noremap = true, silent = true })
 map('n', 't<C-f>', ':noautocmd wa<cr> :TestFile<CR>',    { noremap = true, silent = true })
-map('n', 't<C-a>', ':noautocmd wa<cr> :TestSuite<CR>',   { noremap = true, silent = true })
+map('n', 't<C-s>', ':noautocmd wa<cr> :TestSuite<CR>',   { noremap = true, silent = true })
 map('n', 't<C-l>', ':noautocmd wa<cr> :TestLast<CR>',    { noremap = true, silent = true })
+map('n', 't<C-g>', ':noautocmd wa<cr> :TestVisit<CR>',   { noremap = true, silent = true })
 map('t', '<C-o>',  '<C-\\><C-n>', { noremap = true })
