5258880143470982248409a672b87fd163de1f99
Michael Joseph Kramer <michael@sage.house>
2024-02-17 12:19:27 -0500

    Update to tokyonight configuration

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -4,10 +4,12 @@ return {
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require("tokyonight").setup({
+      transparent = true,
       on_colors = function(colors)
         colors.bg_statusline = colors.none
       end,
+      styles = { floats = "dark" }
     })
-    vim.cmd([[ colorscheme tokyonight-night ]])
+    vim.cmd[[ colorscheme tokyonight-night ]]
   end
 }
