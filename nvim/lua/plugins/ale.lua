af16b004e726e8cdf58f0ae3e3c4ba45b9fc5bd8
Michael Joseph Kramer <michael@sage.house>
2024-03-07 11:27:24 -0500

    Update ALE configuration
    Update ALE configuration to add Python and remove JSX. Also removes
    explict references to linters, which don't seem necessary.

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -5,14 +5,10 @@ return {
     vim.g.ale_fix_on_save = 1
     vim.g.ale_fixers = {
       ['*']              = { 'remove_trailing_lines', 'trim_whitespace' },
+      python             = { 'ruff' },
       javascript         = { 'eslint' },
-      ['javascript.jsx'] = { 'eslint' },
       ruby               = { 'rubocop' },
     }
     vim.g.ale_lint_on_enter = 1
-    vim.g.ale_linters = {
-      lua  = { 'luac', 'luacheck' },
-      ruby = { 'rubocop' }
-    }
   end
 }
